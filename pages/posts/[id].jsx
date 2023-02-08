874012f0adf347156aaf508e2f733979e7fe0f6d
Kai-Itakura <itakai199969@gmail.com>
2023-02-09 02:48:04 +0900

    ✨ getStaticPathsとgetStaticPropsについて学んだ

---
+++ pages/posts/[id].jsx
@@ -0,0 +1,32 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+const Post = ({ postData }) => {
+    return (<Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+    </Layout>
+    );
+}
+
+export const getStaticPaths = async () => {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = ({ params }) => {
+    const postData = getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export default Post;
