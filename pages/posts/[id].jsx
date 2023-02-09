1dd3a4f0f0cc42a9103495da2ed0f65934a85822
Kai-Itakura <itakai199969@gmail.com>
2023-02-09 14:41:22 +0900

    ✨ markdownファイルのレンダリング

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,13 +1,21 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
 
 const Post = ({ postData }) => {
     return (<Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+        <article>
+            <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
     </Layout>
     );
 }
@@ -20,8 +28,8 @@ export const getStaticPaths = async () => {
     }
 }
 
-export const getStaticProps = ({ params }) => {
-    const postData = getPostData(params.id);
+export const getStaticProps = async ({ params }) => {
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
