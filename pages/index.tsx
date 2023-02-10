ac87052c55a26381a3228c41932ea6fd6300f907
Kai-Itakura <itakai199969@gmail.com>
2023-02-10 19:13:44 +0900

    サーバーサイドレンダリングから静的ジェネレートに変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
@@ -6,7 +6,7 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
